a85502362c7601d665d00e89a93e9af63f0e1e0b
Zouheir Nakouzi <zouheir2002@gmail.com>
2025-01-02 23:27:57 +0200

    fixed duplicate task bar items

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "885359341"
+    serviceWorkerVersion: "2810070473"
   }
 });
